d4dff30ff2ff6e260c9bcb0e4bfac8292bcab606
Alexander Kozovski <kozovski.a@gmail.com>
2015-08-08 15:46:06 -0400

    cute puppy successfully listed

--- browser/js/common/directives/campaign/campaign.directive.js
+++ browser/js/common/directives/campaign/campaign.directive.js
@@ -1,6 +1,19 @@
-app.campaign('campaign',function(){
+
+
+app.directive('campaign',function(){
 	return {
 		restrict: 'E',
-		templateUrl: 'js/common/directives/campaign/campaign.html
+		templateUrl: 'js/common/directives/campaign/campaign.html',
+
+        link: function(scope){
+
+            scope.product = {
+                title:'cute puppy',
+                summaryDescription: "well trained and cute",
+                duration: "5",
+                imgUrl: "https://pbs.twimg.com/profile_images/378800000674268962/06ce58cab26c3a0daf80cf57e5acb29b_400x400.jpeg"
+
+            }
+        }
 	}
-})
+});
