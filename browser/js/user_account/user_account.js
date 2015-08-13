6e0a56ad6556ce90aa807c1d75ac970d9a0f1998
Mike <mike.sj.lee@gmail.com>
2015-08-12 21:54:48 -0400

    user account page almost there

--- browser/js/user_account/user_account.js
+++ browser/js/user_account/user_account.js
@@ -1,21 +1,23 @@
-app.config(function ($stateProvider, Session, $http){
+app.config(function ($stateProvider){
 
     $stateProvider.state('user_account', {
         url: '/user_account',
         controller: 'userCtrl',
-        templateUrl: 'js/user_account/user_account.html'
-        // resolve: {
-        //   UserBids: function(Session){
-        //     console.log(Session.user);
-        //       $http.get('/api/bid/user/' & Session.user.id, function(err, bids){
-        //         return {bids: bids}
-        //       })
-        //   }
-        //}
+        templateUrl: 'js/user_account/user_account.html',
+        resolve: {
+          UserBids: function (Session, $http){
+	          //console.log(Session);
+              return $http.get('/api/bids/user/' + Session.user._id).then(function(bids){
+                return bids;
+              })
+	        }
+        }
+
     });
 });
 
-app.controller('userCtrl', function($scope, Session){
+app.controller('userCtrl', function($scope, Session, UserBids){
+    $scope.test = "yo"
     $scope.user = Session.user;
-    //$scope.bids = UserBids.bids;
+    $scope.bids = UserBids.bids;
 })
