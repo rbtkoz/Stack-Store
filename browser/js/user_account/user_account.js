af13f3e546926af7cfc0b00a3f0f9f377b5ca8db
Mike <mike.sj.lee@gmail.com>
2015-08-12 20:42:20 -0400

    user account page details

--- browser/js/user_account/user_account.js
+++ browser/js/user_account/user_account.js
@@ -1,11 +1,17 @@
-app.config(function ($stateProvider){
+app.config(function ($stateProvider, AuthService, Session, $http){
     $stateProvider.state('user_account', {
-        url: '/myaccount',
+        url: '/user_account',
         controller: userCtrl,
-        templateUrl: 'js/user-account/user_account.html'
+        templateUrl: 'js/user-account/user_account.html',
+        resolve: {
+          UserBids: function(Session){
+              $http.get('api/')
+          }
+        }
     })
 });
 
-app.controller('userCtrl', function($scope){
-    $scope.user = {};
+app.controller('userCtrl', function($scope, Session){
+    $scope.user = Session.user;
+
 })
