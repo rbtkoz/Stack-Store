04b8af03ef7e84bfaacba2540b05ac6f6b7a4215
Mike <mike.sj.lee@gmail.com>
2015-08-12 21:03:36 -0400

    basic build

--- browser/js/user_account/user_account.js
+++ browser/js/user_account/user_account.js
@@ -1,17 +1,21 @@
-app.config(function ($stateProvider, AuthService, Session, $http){
+app.config(function ($stateProvider, Session, $http){
+
     $stateProvider.state('user_account', {
         url: '/user_account',
-        controller: userCtrl,
-        templateUrl: 'js/user-account/user_account.html',
-        resolve: {
-          UserBids: function(Session){
-              $http.get('api/')
-          }
-        }
-    })
+        controller: 'userCtrl',
+        templateUrl: 'js/user_account/user_account.html'
+        // resolve: {
+        //   UserBids: function(Session){
+        //     console.log(Session.user);
+        //       $http.get('/api/bid/user/' & Session.user.id, function(err, bids){
+        //         return {bids: bids}
+        //       })
+        //   }
+        //}
+    });
 });
 
 app.controller('userCtrl', function($scope, Session){
     $scope.user = Session.user;
-
+    //$scope.bids = UserBids.bids;
 })
