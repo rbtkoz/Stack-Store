3b41ad2ee4cdf3c3874c210f9275d32716c7cbcd
Stella Zhao <tmrvivian@gmail.com>
2015-08-15 16:00:32 -0400

    styling

--- browser/js/campaign/summary/campaign.ctrl.js
+++ browser/js/campaign/summary/campaign.ctrl.js
@@ -2,5 +2,9 @@ app.controller('CampaignCtrl',function($scope, CampaignFactory, $stateParams){
     CampaignFactory.getAllCampaigns($stateParams.id).then(function (data) {
     	//console.log(data)
         $scope.products = data;
+        $scope.primaryPic = data.imgUrl[0];
+        $scope.changePic=function(i){
+        	$scope.primaryPic=data.imgUrl[i];
+        }
     });
 });
